09bd16109e4c5c433d6add8d3ff94097242487af
Luis Soria <luismiguel.ca@gmail.com>
2016-10-24 12:18:14 +0200

    b
    b

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,4 +1,4 @@
-    function myFunction() {
+  /*  function myFunction() {
         document.getElementById("myDropdown").classList.toggle("show");
     }
 
@@ -14,7 +14,35 @@
           }
         }
       }
-    }
+    } */
+
+
+
+
+  $('#menu-icon').click(function(event){
+      
+      document.getElementById("myDropdown").classList.toggle("show");
+
+    });
+   
+window.onclick = function(event) {
+      if (!event.target.matches('#menu-icon')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    } 
+
+
+
+
+
 
     $(window).scroll(function() {
       if ($(this).scrollTop()>90) {
